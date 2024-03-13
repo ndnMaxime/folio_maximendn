dd2427784e80494258f787214ea4df631b684805
ndnMaxime <155656886+ndnMaxime@users.noreply.github.com>
2024-03-13 12:40:02 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,7 @@
-import { defineConfig } from "astro/config";
-import netlify from '@astrojs/netlify';
-import sitemap from "@astrojs/sitemap";
-import netlify from "@astrojs/netlify";
-import robotsTxt from "astro-robots-txt";
-import UnoCSS from "@unocss/astro";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 
-import solidJs from "@astrojs/solid-js";
-import { remarkReadingTime } from "./src/lib/ remark-reading-time.mjs";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    sitemap(),
-    robotsTxt({
-      sitemap: [
-        "https://gianmarco.xyz/sitemap-index.xml",
-        "https://gianmarco.xyz/sitemap-0.xml",
-      ],
-    }),
-    solidJs(),
-    UnoCSS({ injectReset: true }),
-    icon()
-  ],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
   output: "server",
   adapter: netlify(),
 });
